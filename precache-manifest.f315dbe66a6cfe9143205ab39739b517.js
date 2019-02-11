21d68f594aac925c378c2584c20e955b1d7e7a75
inspectorr <44624950+inspectorr@users.noreply.github.com>
2019-02-12 01:41:30 +0300

    Updates

---
+++ precache-manifest.f315dbe66a6cfe9143205ab39739b517.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6012d80bdf430145ff43",
+    "url": "/simpledraw/static/js/runtime~main.6012d80b.js"
+  },
+  {
+    "revision": "0e41b86c65cc32576ac8",
+    "url": "/simpledraw/static/js/main.0e41b86c.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/simpledraw/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "0e41b86c65cc32576ac8",
+    "url": "/simpledraw/static/css/main.37dba701.chunk.css"
+  },
+  {
+    "revision": "d3d2aa4ca31239b23a84ec5d591cdea2",
+    "url": "/simpledraw/index.html"
+  }
+];
